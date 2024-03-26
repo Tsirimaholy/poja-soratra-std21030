eed3eca6b00e084c0a269a84df1b1f2d40b128f9
Tsirimaholy <hei.tsirimaholy@gmail.com>
2024-03-26 09:20:52 +0300

    feat: adding all constructor args and no args

--- src/main/java/hei/school/soratra/repository/model/Soratra.java
+++ src/main/java/hei/school/soratra/repository/model/Soratra.java
@@ -2,13 +2,17 @@ package hei.school.soratra.repository.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Repository;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Soratra {
     @Id
     private String id;
